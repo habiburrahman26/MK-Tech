3d7e5354d6e5cc37b474b8c790b0e1fb2567eb1f
HabiburRahmanAnik <habib264521@gmail.com>
2022-06-07 15:58:53 +0600

    add another section

--- js/script.js
+++ js/script.js
@@ -17,3 +17,17 @@ tabContainers.addEventListener('click', function (e) {
     .querySelector(`.tab-content--${clicked.dataset.tab}`)
     .classList.remove('hidden');
 });
+
+var swiper = new Swiper('.mySwiper', {
+  slidesPerView: 2,
+  spaceBetween: 40,
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
